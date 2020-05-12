21e3f0cb8411f3bc83a9daf0f8b1e8ee66bf44c5
asensts <asensts@163.com>
2020-05-12 17:17:33 +0800

    submit header

---
+++ src/views/home/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <Header></Header>
+  </div>
+</template>
+
+<script>
+import Header from '../header/header.vue';
+
+export default {
+  components: {
+    Header
+  }
+};
+</script>
+
+<style lang="scss">
+</style>
