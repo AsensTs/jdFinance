2dfdc1d205a4e4cb20c17fce7538918ae5b665ca
asensts <asensts@163.com>
2020-05-12 09:28:04 +0800

    btn panel update

---
+++ src/views/core/panel.vue
@@ -0,0 +1,33 @@
+<template>
+    <section :class='[panel,customize]'>
+        <h4>-{{ title }}-</h4>
+        <slot/>
+    </section>
+</template>
+
+<script>
+export default {
+  props: {
+    customize: {
+      type: String,
+      default: ''
+    },
+    title: {
+      type: String,
+      default: ''
+    }
+  },
+  data() {
+    return {
+      defaultPanel: panel
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import '../assets/style/mixin.scss';
+.panel {
+  @include panel;
+}
+</style>
